905823826e0422e84a1910020e0fe74e8611eec1
BadCuzMad <72146849+BadCuzMad@users.noreply.github.com>
2022-08-10 17:08:44 +0400

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -2,9 +2,9 @@ import streamlit
 
 streamlit.title('Breakfast favorites')
 streamlit.header('Breakfast Menu')
-streamlit.text(🥣 'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥗 'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🐔 'Hard-Boiled Free-Range Egg')
-streamlit.text(🥑🍞 'Avocado toast')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
